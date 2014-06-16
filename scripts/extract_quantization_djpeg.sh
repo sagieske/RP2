c89c478899976fd1eafcf46e4f0c66874b955949
Sharon Gieske <sharongieske@gmailcom>
2014-06-16 13:18:35 +0200

    calculate time script run

--- scripts/extract_quantization_djpeg.sh
+++ scripts/extract_quantization_djpeg.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
+
+# Get time as a UNIX timestamp (seconds elapsed since Jan 1, 1970 0:00 UTC)
+T="$(date +%s)"
+
 for image in /images/*.JPG
 do
 outputfilename=$image.output
 djpeg -verbose -verbose $image 2>&1 | cat > $outputfilename
 python parsefile_djpeg.py $outputfilename
 done
+
+# Print elapsed time
+T="$(($(date +%s)-T))"
+echo "Time in seconds: ${T}"
+
+printf "Pretty format: %02d:%02d:%02d:%02d\n" "$((T/86400))" "$((T/3600%24))" "$((T/60%60))" "$((T%60))""
 
