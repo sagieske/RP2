704fc8321e8dca459071fd66be817946382c1a8d
sagieske <sharongieske@gmail.com>
2014-06-17 15:13:30 +0200

    try catch for reading file

--- scripts/parsefile_djpeg.py
+++ scripts/parsefile_djpeg.py
@@ -19,26 +19,28 @@ deviceid = camerainfo[2]
 uniqueID = camerainfo[3]
 
 # read file
-with open(INPUTFILE) as f:
-    data=f.read(1500)
+try:
+	with open(INPUTFILE) as f:
+	    data=f.read(1500)
 
-# get all dqt rows
-dqt_values = re.findall(r'\s{3,4}\d{1,2}', data)
-
-# only extract & dump if values are found
-if dqt_values:
-	# extract tables to array
-	dqt1 = []
-	dqt2 = []
-	for j in range(0,16):
-		row = map(int, dqt_values[j*8:j*8+8])
-		if j < 9:
-			dqt1.append(row)
-		else:
-			dqt2.append(row)
-	# pickle dump camera data and quantization tables
-	pickle.dump( (camerainfo, dqt1, dqt2), open( OUTPUTFILE, "wb" ) )
-
+	# get all dqt rows
+	dqt_values = re.findall(r'\s{3,4}\d{1,2}', data)
+	
+	# only extract & dump if values are found
+	if dqt_values:
+		# extract tables to array
+		dqt1 = []
+		dqt2 = []
+		for j in range(0,16):
+			row = map(int, dqt_values[j*8:j*8+8])
+			if j < 9:
+				dqt1.append(row)
+			else:
+				dqt2.append(row)
+		# pickle dump camera data and quantization tables
+		pickle.dump( (camerainfo, dqt1, dqt2), open( OUTPUTFILE, "wb" ) )
+except: 
+	print "no such file"
 
 
 
