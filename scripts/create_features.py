2b09b08fe04f1b074ab0bc750c30cc61459fc1f8
sagieske <sharongieske@gmail.com>
2014-06-20 22:18:52 +0200

    merge

--- scripts/create_features.py
+++ scripts/create_features.py
@@ -45,11 +45,11 @@ class Create_features(object):
 			camera = re.sub('/images/','',camerainfo[0])
 			identifier = (camera, camerainfo[1])
 			dqts = [infotuple[1], infotuple[2]]
-			# known make & model
+			# known make & modeli
 			if identifier in self.camera_dict:
 				# value already in dictionary
 				if dqts in self.camera_dict[identifier]:
-					pass
+					print "same dqt"
 				# new value, same identifier
 				else:
 					value = self.camera_dict[identifier]
