5521834df2c4c31dadd34348c3e868e8afabb060
Sharon Gieske <sharongieske@gmailcom>
2014-06-20 22:17:07 +0200

    debug. parsing file created tables of 9 and 7 instead of 8,8

--- scripts/parsefile_djpeg.py
+++ scripts/parsefile_djpeg.py
@@ -21,7 +21,7 @@ uniqueID = camerainfo[3]
 # read file
 try:
 	with open(INPUTFILE) as f:
-	    data=f.read(1500)
+	    data=f.read(1600)
 
 	# get all dqt rows
 	dqt_values = re.findall(r'\s{3,4}\d{1,2}', data)
@@ -33,12 +33,14 @@ try:
 		dqt2 = []
 		for j in range(0,16):
 			row = map(int, dqt_values[j*8:j*8+8])
-			if j < 9:
+			if j < 8:
 				dqt1.append(row)
 			else:
 				dqt2.append(row)
 		# pickle dump camera data and quantization tables
 		pickle.dump( (camerainfo, dqt1, dqt2), open( OUTPUTFILE, "wb" ) )
+	else:
+		print "no DQT found for file %s" %(INPUTFILE)
 except: 
 	print "no such file"
 
