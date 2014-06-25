aeaebfeece5c143df7fac201333ff693cb019103
Sharon Gieske <sharongieske@gmailcom>
2014-06-25 14:05:19 +0200

    merge

--- scripts/create_features.py
+++ scripts/create_features.py
@@ -14,6 +14,7 @@ import operator
 from sklearn import tree
 from sklearn.externals.six import StringIO
 import pydot
+import sklearn
 from sklearn import cross_validation
 from pprint import pformat 
 import hashlib
